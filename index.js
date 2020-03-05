f91aacd120b8f455cdbd05dbe0b17d89fb8abe2c
Lawrence Baker <lbaker@demosphere.com>
2020-03-05 10:20:20 -0500

    resolve actual cost prior to response

--- index.js
+++ index.js
@@ -2,30 +2,37 @@ const { ApolloServer } = require('apollo-server')
 const { makeExecutableSchema } = require('graphql-tools')
 const { createComplexityLimitRule } = require('graphql-validation-complexity')
 
+const context = require('./src/context')
+const logUsage = () => {}
+const maxComplexity = 10
 
-const Context = require('./src/context')
-const CostCalculator = require('./src/calculator')
-
-const ComplexityLimitRule = createComplexityLimitRule(
-  10,
-  {
-    onCost: (cost) => {
-      console.log('query cost:', cost);
-    },
-    introspectionListFactor: 0,
-    formatErrorMessage: cost => (
-      `query with cost ${cost} exceeds complexity limit`
-    )
-  }
-)
-const items = [
-  { id: 1, name: 'Apple', plu: '#1' },
-  { id: 2, name: 'Banana', plu: '#2' },
-  { id: 3, name: 'Carrot', plu: '#3' },
-  { id: 4, name: 'Dates', plu: '#4' },
-  { id: 5, name: 'Eggs', plu: '#5' },
-  { id: 6, name: 'Fanta', plu: '#6' }
+const validationRules = [
+  createComplexityLimitRule(
+    maxComplexity,
+    {
+      formatErrorMessage: cost => `query with cost ${cost} exceeds complexity limit`,
+      introspectionListFactor: 0,
+      onCost: (cost) => console.log('query cost:', cost)
+    }
+  )
 ]
+
+const items = [
+  { id: 1, name: 'Apple', plu: '#1', vendorId: 1 },
+  { id: 2, name: 'Banana', plu: '#2', vendorId: 1 },
+  { id: 3, name: 'Carrot', plu: '#3', vendorId: 2 },
+  { id: 4, name: 'Dates', plu: '#4', vendorId: 2 },
+  { id: 5, name: 'Eggs', plu: '#5', vendorId: 3 },
+  { id: 6, name: 'Fanta', plu: '#6', vendorId: 4 }
+]
+
+const vendors = [
+  { id: 1, name: 'Mars' },
+  { id: 2, name: 'Nestle' },
+  { id: 3, name: 'Dole' },
+  { id: 4, name: 'Hershey' }
+]
+
 const typeDefs = `
   directive @cost(value: Int) on FIELD_DEFINITION | OBJECT | FIELD
   directive @costFactor(value: Int) on FIELD_DEFINITION
@@ -38,18 +45,23 @@ const typeDefs = `
     id: Int
     name: String
     plu: String
+    vendor: Vendor
+  }
+
+  type Vendor @cost(value: 1) {
+    id: Int
+    name: String
   }
 `
 
 const resolvers = {
+  Item: {
+    vendor: item => vendors.find(vendor => vendor.id === item.vendorId)
+  },
   Query: {
     item: (_obj, { id }) => items.find(item => item.id === id),
     items: () => items
   }
-}
-
-const schemaDirectives = {
-  cost: CostCalculator
 }
 
 const schema = makeExecutableSchema({
@@ -58,10 +70,17 @@ const schema = makeExecutableSchema({
   schemaDirectives
 })
 
+const plugins = [{
+  requestDidStart: () => ({
+    willSendResponse: ({ context, document, response }) => {
+      logUsage({ context, document, response, schema })
+    }
+  })
+}]
+
 const server = new ApolloServer({
-  context: Context,
-  schema,
-  validationRules: [ComplexityLimitRule]
+  context, schema, plugins, validationRules,
+  tracing: true
 })
 
 server.listen(4001)
